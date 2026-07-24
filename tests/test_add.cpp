f76deb74ae0d5859e1e6e21ea3476b0b67eea8f8
李孙洋 <765836056@qq.com>
2026-07-24 10:52:38 +0800

    修改错误

--- tests/test_add.cpp
+++ tests/test_add.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 int main() {
-    if (add(2, 3) != 6) {
+    if (add(2, 3) != 5) {
         std::cerr << "FAIL: add(2,3) expected 5" << std::endl;
         return 1;   // 非 0 返回值 = 测试失败
     }
